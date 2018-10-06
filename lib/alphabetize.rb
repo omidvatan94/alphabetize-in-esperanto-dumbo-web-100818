6cc1eaefae4daa80df5daff4c8183e1fd7bf4ee5
Omid Vatandoost <omidvatan94@gmail.com>
2018-10-06 15:36:04 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esp_alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  
+arr.sort_by{|string| string.tr(esp_alph, ascii)}
+   
 end
+
